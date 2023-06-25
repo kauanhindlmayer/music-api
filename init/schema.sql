9ea4a48a05d3e278198d26bdfbf43241dae19368
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-06-25 17:20:44 -0300

    feat: added triggers

--- init/schema.sql
+++ init/schema.sql
@@ -40,3 +40,59 @@ CREATE TABLE IF NOT EXISTS `music_api_database`.`subscriptions` (
   `modified_at` timestamp NOT NULL,
   PRIMARY KEY (`id`)
 )
+
+-- Ensure that the genre_id column references an existing id in the genres table
+DELIMITER //
+
+CREATE TRIGGER musics_genre_id_trigger
+BEFORE INSERT ON `music_api_database`.`musics`
+FOR EACH ROW
+BEGIN
+  IF NEW.genre_id IS NOT NULL AND NOT EXISTS (SELECT 1 FROM `music_api_database`.`genres` WHERE id = NEW.genre_id) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Foreign key constraint violation: Invalid genre_id.';
+  END IF;
+END//
+
+DELIMITER ;
+
+-- Ensure that the release_date is not in the future
+DELIMITER //
+
+CREATE TRIGGER musics_release_date_trigger
+BEFORE INSERT ON `music_api_database`.`musics`
+FOR EACH ROW
+BEGIN
+  IF NEW.release_date > NOW() THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Release date cannot be in the future.';
+  END IF;
+END//
+
+DELIMITER ;
+
+-- Ensure that the expire_date is greater than the created_at
+DELIMITER //
+
+CREATE TRIGGER record_label_expire_date_trigger
+BEFORE INSERT ON `music_api_database`.`record_label`
+FOR EACH ROW
+BEGIN
+  IF NEW.expire_date <= NEW.created_at THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Expire date must be greater than created_at.';
+  END IF;
+END//
+
+DELIMITER ;
+
+-- Ensure that the value is greater than 0
+DELIMITER //
+
+CREATE TRIGGER subscriptions_value_trigger
+BEFORE INSERT ON `music_api_database`.`subscriptions`
+FOR EACH ROW
+BEGIN
+  IF NEW.value <= 0 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Value must be greater than 0.';
+  END IF;
+END//
+
+DELIMITER ;
