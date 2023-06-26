182645c7bb84121cf03a6aff51aef29f8c8352be
Jéferson Salomon <sr.gibbles@gmail.com>
2023-06-26 15:00:28 -0300

    artist table CRUD

---
+++ src/main/routes/artists.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+from infra.db.settings.connection import DBConnectionHandler
+from services.artist_service import ArtistService
+
+database = DBConnectionHandler()
+database.__enter__()
+artist_service = ArtistService(database)
+
+artists_bp = Blueprint('artists', __name__)
+
+@artists_bp.route('/artists', methods=['GET'])
+def get_all_artists():
+    return artist_service.get_all()
+
+@artists_bp.route('/artists', methods=['POST'])
+def add_artist():
+    return artist_service.add()
+
+@artists_bp.route('/artists/<int:id>', methods=['GET'])
+def get_artist_by_id(id):
+    return artist_service.get_by_id(id)
+
+@artists_bp.route('/artists/<int:id>', methods=['PUT'])
+def update_artist(id):
+    return artist_service.update(id)
+
+@artists_bp.route('/artists/<int:id>', methods=['DELETE'])
+def delete_artist(id):
+    return artist_service.delete(id)
