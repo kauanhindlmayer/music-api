4007e98a489f653868f3a78ab4b749b4e514035c
Arthur Hoengen <arthurhoengen@gmail.com>
2023-06-26 21:25:46 -0300

    feat: customer crud

--- src/main/routes/api_routes.py
+++ src/main/routes/api_routes.py
@@ -4,6 +4,7 @@ from main.routes.genres import genres_bp
 from main.routes.record_labels import record_labels_bp
 from main.routes.musics import music_bp
 from main.routes.artists import artists_bp
+from main.routes.customers import customers_bp
 
 from infra.db.settings.connection import DBConnectionHandler
 from infra.db.settings.base import Base
@@ -18,3 +19,4 @@ app.register_blueprint(genres_bp)
 app.register_blueprint(record_labels_bp)
 app.register_blueprint(music_bp)
 app.register_blueprint(artists_bp)
+app.register_blueprint(customers_bp)
